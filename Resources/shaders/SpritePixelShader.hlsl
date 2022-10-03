b0d73a2d24e5786980ae3a630ea786a351e5c0ff
LE2A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-10-03 13:33:34 +0900

    test

--- Resources/shaders/SpritePixelShader.hlsl
+++ Resources/shaders/SpritePixelShader.hlsl
@@ -5,5 +5,5 @@ SamplerState smp : register(s0);      // 0番スロットに設定されたサ�
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	return tex.Sample(smp, input.uv) * color;
+	return tex.Sample(smp, input.uv) * color;//
 }
